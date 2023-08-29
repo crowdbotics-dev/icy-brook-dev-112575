464c24d5cc45262bd47c77d039f9f88b7c13e2d8
Crowdbotics <engineering@crowdbotics.com>
2023-08-29 08:16:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-brook-dev-112575",
+  url: "https://icy-brook-dev-112575.botics.co",
+  api: "https://icy-brook-dev-112575.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
